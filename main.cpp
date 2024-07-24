fd129ca0bf8de0cb581913c7d83fad03028991f9
column-jam <160190489+column-jam@users.noreply.github.com>
2024-07-24 10:56:09 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -37,16 +37,16 @@ void initialize_code() {
 	for (int i = 0; i < room_amount; i++) {
 		color_maker(room_color[i]);
 	}
-}
+}	
 
-void print_room(int room_color[height][width], string room_layout[height]) {
+void print_room() {
 	if (maze_flag) return; //Disabling the standard print process for the maze room, that has its own function
 	for (int i = 0; i < height; i++) {
 		string colored_string = "\0";
 		for (int j = 0; j < width; j++) {
-			switch (room_layout[i][j]) {
+			switch (room_layout[room_number][i][j]) {
                 case '#':        //Concatenating the color codes to the string based on what char is in each cell
-                    if (room_color[i][j] == 0) {
+                    if (room_color[room_number][i][j] == 0) {
                     	colored_string += GRAY;
 						break;
 					}
@@ -68,7 +68,7 @@ void print_room(int room_color[height][width], string room_layout[height]) {
                     }
                     break;
             }
-            colored_string += room_layout[i][j]; //Combine the color code with the actual character
+            colored_string += room_layout[room_number][i][j]; //Combine the color code with the actual character
 		}                             
 		std::cout << colored_string;
 		if (i != height - 1) std::cout << "\n";
@@ -92,29 +92,29 @@ int check_adjacent(char symbol, int y_pos, int x_pos) {
 }
 
 void maze_functionality() {
-    char approximately_adjacent[7][7];
+    char lit_maze_tiles[5][7];
 	for (int i = 0; i < 7; i++) {
 	    for (int j = 0; j < 7; j++) {
-	        int row = i + y_pos - 3;
+	        int row = i + y_pos - 2;
 	        int col = j + x_pos - 3;
 	        if (row >= 0 && row < height && col >= 0 && col < width) {
-	            approximately_adjacent[i][j] = room_layout[room_number][row][col];
+	            lit_maze_tiles[i][j] = room_layout[room_number][row][col];
 	        }
 	    }
 	}
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            if ((y >= y_pos - 3 && y <= y_pos + 3) && (x >= x_pos - 3 && x <= x_pos + 3)) {
-                int i = y - (y_pos - 3);  //Centers i and j on the top left corner of the player's square
+            if ((y >= y_pos - 2 && y <= y_pos + 2) && (x >= x_pos - 3 && x <= x_pos + 3)) {
+                int i = y - (y_pos - 2);  //Centers i and j on the top left corner of the player's square
                 int j = x - (x_pos - 3);
-                if (i == 3 && j == 3) {
+                if (i == 2 && j == 3) {
                     std::cout << WHITE << "@";
-                } else if (i == 0 || i == 6 || j == 0 || j == 6) {
-                    std::cout << GRAY << approximately_adjacent[i][j]; //Gray for the inner ring, light gray for the outer ring, white for the player. Gives the impression of a light centered on the player
-                } else if (i == 1 || i == 5 || j == 1 || j == 5){
-                    std::cout << GRAY_BUT_LIGHTER << approximately_adjacent[i][j];
+                } else if (i == 0 || i == 5 || j == 0 || j == 6) {
+                    std::cout << GRAY << lit_maze_tiles[i][j]; //Gray for the inner ring, light gray for the outer ring, white for the player. Gives the impression of a light centered on the player
+                } else if (i == 1 || i == 4 || j == 1 || j == 5){
+                    std::cout << GRAY_BUT_LIGHTER << lit_maze_tiles[i][j];
                 } else {
-                	std::cout << LIGHT_GRAY << approximately_adjacent[i][j];
+                	std::cout << LIGHT_GRAY << lit_maze_tiles[i][j];
 				}
             } else {
                 std::cout << " ";
@@ -124,44 +124,91 @@ void maze_functionality() {
     }
 }
 
-
-void laser_path() {
-	light_y_pos = 21;
-	light_x_pos = 60;
-	light_direction = 1;
-	while (1) {
-		room_layout[room_number][light_y_pos][light_x_pos] = (light_direction == 1 || light_direction == 3) ? '|' : '-'; //If light direction is up or down, print |, otherwise print -
-		setCursorPosition(0, 0);
-		print_room(room_color[room_number], room_layout[room_number]);
-		std::cout << "\033[?25l";
-		Sleep(50);
-		direction_one = check_adjacent('/', light_y_pos, light_x_pos);
-		direction_two = check_adjacent('\\', light_y_pos, light_x_pos);
-		if (direction_one == light_direction || direction_two == light_direction) { //If there's a mirror in the light's way, trigger code
-		    switch (light_direction) {
-		        case 1: (direction_one == 1) ? (light_direction = 2, light_y_pos--, light_x_pos++) : (light_direction = 4, light_y_pos--, light_x_pos--); break; //Change light's direction and position based on the mirror it hits and what direction it's going
-		        case 2: (direction_one == 2) ? (light_direction = 1, light_y_pos--, light_x_pos++) : (light_direction = 3, light_y_pos++, light_x_pos++); break;
-		        case 3: (direction_one == 3) ? (light_direction = 4, light_y_pos++, light_x_pos--) : (light_direction = 2, light_y_pos++, light_x_pos++); break;
-		        case 4: (direction_one == 4) ? (light_direction = 3, light_y_pos++, light_x_pos--) : (light_direction = 1, light_y_pos--, light_x_pos--); break;
-		    }
-		} else {
-			switch(light_direction) { //If nothing is in the light's way, it carries on in its direction
-				case 1: light_y_pos--; break;
-				case 2: light_x_pos++; break;
-				case 3: light_y_pos++; break;
-				case 4: light_x_pos--; break;
+class Laser {
+	private:
+		int y = 21;
+		int x = 60;
+		int direction = 1;
+		
+		bool isLaserActive = 1;
+		bool isDoorDown = 0;
+		
+		void print() {
+			room_layout[room_number][y][x] = (direction == 1 || direction == 3) ? '|' : '-'; //If light direction is up or down, print |, otherwise print -
+			setCursorPosition(0, 0);
+			print_room();
+			std::cout << "\033[?25l";
+			Sleep(50);
+		}
+		void change_direction() {
+			int direction_one = check_adjacent('/', y, x);
+			int direction_two = check_adjacent('\\', y, x);
+			if (direction_one == direction || direction_two == direction) { //If there's a mirror in the light's way, trigger code
+			    switch (direction) {
+			        case 1: (direction_one == 1) ? (direction = 2, y--, x++) : (direction = 4, y--, x--); break; //Change light's direction and position based on the mirror it hits and what direction it's going
+			        case 2: (direction_one == 2) ? (direction = 1, y--, x++) : (direction = 3, y++, x++); break;
+			        case 3: (direction_one == 3) ? (direction = 4, y++, x--) : (direction = 2, y++, x++); break;
+			        case 4: (direction_one == 4) ? (direction = 3, y++, x--) : (direction = 1, y--, x--); break;
+			    }
+			} else {
+				switch(direction) { //If nothing is in the light's way, it carries on in its direction
+					case 1: y--; break;
+					case 2: x++; break;
+					case 3: y++; break;
+					case 4: x--; break;
+				}
 			}
 		}
-		if (check_adjacent('X', light_y_pos + 1, light_x_pos)) flag_laser_door_down = 1; //If light hits door, flag gets triggered
-		if (check_adjacent('#', light_y_pos, light_x_pos) || check_adjacent('-', light_y_pos, light_x_pos) == light_direction || check_adjacent('|', light_y_pos, light_x_pos) == light_direction || check_adjacent('$', light_y_pos, light_x_pos) == light_direction || flag_laser_door_down == 1) {
+		bool isLaserBlocked() {
+			return	
+			check_adjacent('#', y, x) ||
+			check_adjacent('-', y, x) == direction ||
+			check_adjacent('|', y, x) == direction;
+		};
+		void reset() {
+			print();
 			for (int i = 0; i < height; i++) {
 			    room_layout[room_number][i] = room_two_but_technically_the_third_copy[i]; //Reset the room if the beam fails
 			}
-			if (flag_laser_door_down) room_layout[room_number][2] = "########################################################         #######################################################"; //Remove door
-			break;
+			isLaserActive = 0;
 		}
-	}	
-}
+		void break_door() {
+			room_layout[room_number][2] = "########################################################         #######################################################"; //Remove door
+			isDoorDown = 1;
+		}
+		
+	public:
+		void update() {
+			print();
+			change_direction();
+			if (isLaserBlocked()) reset();
+			if (check_adjacent('X', y + 1, x)) break_door();
+		}
+		bool get_is_laser_active() {
+			return isLaserActive;	
+		}
+		bool get_is_door_down() {
+			return isDoorDown;
+		}
+};
+
+class Mirror {
+	private:
+		
+	public:
+		int check_direction(char symbol) {
+			return check_adjacent(symbol, y_pos, x_pos);
+		}
+		void turn(int direction, char symbol) {
+			switch(direction) {
+				case 1: room_layout[room_number][y_pos - 1][x_pos] = symbol; break;
+				case 2: room_layout[room_number][y_pos][x_pos + 1] = symbol; break;
+				case 3: room_layout[room_number][y_pos + 1][x_pos] = symbol; break;
+				case 4: room_layout[room_number][y_pos][x_pos - 1] = symbol; break;
+			}
+			flag_mirror_turned = 1;
+		}
+};
 
 void room_interactions() {
 	switch(room_number) {
@@ -175,7 +222,7 @@ void room_interactions() {
 				flag_second_room_button_one = 1;
 				room_layout[room_number][23] = "###########################                              *Click!                             ###########################";
 				setCursorPosition(0, 0);
-				print_room(room_color[room_number], room_layout[room_number]);
+				print_room();
    				std::cout << "\033[?25l";
 				Sleep(500);
 				room_layout[room_number][23] = "###########################                                                                  ###########################";
@@ -184,40 +231,28 @@ void room_interactions() {
 				flag_second_room_button_two = 1;
 				room_layout[room_number][23] = "###########################                              *Click!                             ###########################";
 				setCursorPosition(0, 0);
-				print_room(room_color[room_number], room_layout[room_number]);
+				print_room();
    				std::cout << "\033[?25l";
 				Sleep(500);
 				room_layout[room_number][23] = "###########################                                                                  ###########################";
 			}
 			break;
 		case 2:
-			direction_one = check_adjacent('/', y_pos, x_pos); //Checking whether there's a / or \ present, then swapping them
-			if (direction_one != 0) flag_mirror_turned = 1;
-			if (direction_one == 1) {
-				room_layout[room_number][y_pos - 1][x_pos] = '\\';
-			} else if (direction_one == 2) {
-				room_layout[room_number][y_pos][x_pos + 1] = '\\';
-			} else if (direction_one == 3) {
-				room_layout[room_number][y_pos + 1][x_pos] = '\\';
-			} else if (direction_one == 4) {
-				room_layout[room_number][y_pos][x_pos - 1] = '\\';
-			} else {
-				direction_two = check_adjacent('\\', y_pos, x_pos); //Nesting this in an 'else', otherwise it would change / to \, then \ to / again
-				if (direction_two == 1) {
-					room_layout[room_number][y_pos - 1][x_pos] = '/';
-				} else if (direction_two == 2) {
-					room_layout[room_number][y_pos][x_pos + 1] = '/';
-				} else if (direction_two == 3) {
-					room_layout[room_number][y_pos + 1][x_pos] = '/';
-				} else if (direction_two == 4) {
-					room_layout[room_number][y_pos][x_pos - 1] = '/';
-				}
+			Mirror mirror;
+			if (mirror.check_direction('/')) {  //Checking whether there's a / or \ present, then swapping them
+				mirror.turn(mirror.check_direction('/'), '\\');
+			} else if (mirror.check_direction('\\')) {
+				mirror.turn(mirror.check_direction('\\'), '/');
 			}
+			
 			if (check_adjacent('O', y_pos, x_pos)) {
 				for (int i = 0; i < height; i++) {
 			    	room_two_but_technically_the_third_copy[i] = room_layout[room_number][i]; //Store the room before the laser activates
 				}
-				laser_path();
+				Laser laser;
+				while (laser.get_is_laser_active()) {
+					laser.update();
+				}
 			}
 			break;
 		case 3:
@@ -267,19 +302,17 @@ void room_updates(int* x_pos, int* y_pos) {
 			}
 			break;
 		case 2:
-			if (check_adjacent('^', *y_pos, *x_pos) && !flag_laser_door_down) {
+			if (check_adjacent('^', *y_pos, *x_pos)) {
 				room_layout[room_number][9] = "###########################             *A laser? Maybe it can break that door...            ###########################";
    				flag_third_room_light_text = 1;
 			} else if (flag_third_room_light_text == 1) {
 				room_layout[room_number][9] = "###########################                                                                  ###########################";
 				flag_third_room_light_text = 0;
 			}
-			if ((check_adjacent('/', *y_pos, *x_pos) || check_adjacent('\\', *y_pos, *x_pos)) && flag_mirror_turned == 0) {
+			if ((check_adjacent('/', *y_pos, *x_pos) || check_adjacent('\\', *y_pos, *x_pos)) && !flag_mirror_turned) {
 				room_layout[room_number][9] = "###########################                 *I think I can turn that mirror                  ###########################";
-				flag_third_room_mirror_text = 1;
-			} else if (flag_third_room_mirror_text == 1) {
+			} else if (room_layout[room_number][9][44] == '*' && (flag_mirror_turned || !check_adjacent('/', *y_pos, *x_pos) || !check_adjacent('\\', *y_pos, *x_pos))) {
 				room_layout[room_number][9] = "###########################                                                                  ###########################";
-				flag_third_room_mirror_text = 0;
 			}
 			if (*x_pos <= 64 && *x_pos >= 56 && *y_pos == 0) {
 				room_number++;
@@ -299,27 +332,25 @@ void room_updates(int* x_pos, int* y_pos) {
 	}
 }
 
-void player_action(int *x_pos, int *y_pos, string room_layout[height]) {
+void player_action(int *x_pos, int *y_pos) {
 	temp_x = *x_pos;
 	temp_y = *y_pos;
-	while (temp_x == *x_pos && temp_y == *y_pos) { //While the player hasn't moved...
-		if (kbhit()) {
+	while (1) {
+		if (kbhit()) { // If the player presses a button
 			char user_input = _getch();
-			if ((user_input == 119 || user_input == 72) && room_layout[*y_pos - 1][*x_pos] == ' ' && *y_pos != 0) {                 //W Up
+			if ((user_input == 119 || user_input == 72) && room_layout[room_number][*y_pos - 1][*x_pos] == ' ' && *y_pos != 0) {                 //W Up
 				(*y_pos)--;
-			} else if ((user_input == 115 || user_input == 80) && room_layout[*y_pos + 1][*x_pos] == ' ' && *y_pos != 29) {           //S Down
+			} else if ((user_input == 115 || user_input == 80) && room_layout[room_number][*y_pos + 1][*x_pos] == ' ' && *y_pos != 29) {           //S Down
 				(*y_pos)++;
-			} else if ((user_input == 100 || user_input == 77) && room_layout[*y_pos][*x_pos + 1] == ' ') {           //D Right
+			} else if ((user_input == 100 || user_input == 77) && room_layout[room_number][*y_pos][*x_pos + 1] == ' ') {           //D Right
 				(*x_pos)++;
-			} else if ((user_input == 97 || user_input == 75) && room_layout[*y_pos][*x_pos - 1] == ' ') {            //A Left
+			} else if ((user_input == 97 || user_input == 75) && room_layout[room_number][*y_pos][*x_pos - 1] == ' ') {            //A Left
 				(*x_pos)--;
 			} else if (user_input == 101) { //E interact
 				room_interactions();
-		    	setCursorPosition(0, 0);
-				print_room(room_color[room_number], room_layout);
-   				std::cout << "\033[?25l";
 			}
 			room_updates(x_pos, y_pos);
+			break;
 		}
 	}
 	
@@ -330,9 +361,9 @@ int main(void) {
 	
     while (1) {
     	setCursorPosition(0, 0);
-		print_room(room_color[room_number], room_layout[room_number]);
+		print_room();
    		std::cout << "\033[?25l";
-		player_action(&x_pos, &y_pos, room_layout[room_number]);
+		player_action(&x_pos, &y_pos);
 	}
 
 	return 0;
